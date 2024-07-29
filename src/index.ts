2085fb19b9717fa804b6a3bb8097b032f40f22c8
softsamiul <softsamiul@gmail.com>
2024-07-29 21:25:56 +0530

    function practice code added

--- src/index.ts
+++ src/index.ts
@@ -32,4 +32,32 @@ const user: {
   company: "MI",
 };
 
-user.company = "something"; // return a error Cannot assign to 'company' because it is a read-only property.
+//user.company = "something"; // return a error Cannot assign to 'company' because it is a read-only property.
+
+// -------- Learning Function --------
+// Normal function
+// Arrow function
+
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+add(1, 2);
+
+const arrFunction = (n1: number, n2: number): number => {
+  return n1 + n2;
+};
+arrFunction(2, 4);
+
+//---- object ---> Function ---> method
+const dummyUser = {
+  name: "Mr X",
+  balance: 0,
+  addBalance(balance: number): number {
+    return this.balance + balance;
+  },
+};
+// array map with type
+const arrN: number[] = [1, 2];
+const newArrN: number[] = arrN.map((elem: number): number => elem * elem);
+
+// --------- Spread and Rest Operator ---------
