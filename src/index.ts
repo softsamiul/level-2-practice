530b6ec72d6a8697cf2b0e3bfc75e2c3ec05c645
softsamiul <softsamiul@gmail.com>
2024-07-30 08:46:29 +0530

    spread, rest operator and array and object destructuring

--- src/index.ts
+++ src/index.ts
@@ -60,4 +60,49 @@ const dummyUser = {
 const arrN: number[] = [1, 2];
 const newArrN: number[] = arrN.map((elem: number): number => elem * elem);
 
-// --------- Spread and Rest Operator ---------
+// --------- Spread and Rest Operator and destructuring ---------
+const bros: string[] = ["Mr X", "Mr Y", "Mr Z"];
+const bros2: string[] = ["Mr A", "Mr B", "Mr C"];
+// bros.push(bros2) return a error Argument of type 'string[]' is not assignable to parameter of type 'string'
+bros.push(...bros2); // will return a new array with both array string
+
+const mentors1 = {
+  frontend: "Sam",
+};
+const mentor2 = {
+  backend: "Sun",
+};
+// spread
+const mentors = { ...mentors1, ...mentor2 };
+// rest
+const greet = (...participiants: string[]) => {
+  participiants.forEach((p: string) => console.log(`Hi ${p}`));
+};
+
+// object destructuring
+const info: {
+  nameInfo: string;
+  job_detail: {
+    role: "SDE";
+    company: string;
+  };
+  age: number;
+} = {
+  nameInfo: "Mr T",
+  job_detail: {
+    role: "SDE",
+    company: "X",
+  },
+  age: 25,
+};
+
+const {
+  nameInfo,
+  age,
+  job_detail: { role },
+} = info;
+
+// array destructring
+const myFds: string[] = ["X", "Y", "Z"];
+
+const [a, b, bestfriend] = myFds; // here Z will be bestfrind
