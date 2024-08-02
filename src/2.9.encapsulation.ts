a374a378740a91763896fcbb4cc111ed1fd595b6
softsamiul <softsamiul@gmail.com>
2024-08-02 21:32:06 +0530

    encapsulation short notes added

---
+++ src/2.9.encapsulation.ts
@@ -0,0 +1,3 @@
+{
+  // Encapsulation ---> Encapsulation is nothing it's just controlling the value using access modifier like which properties is accessiable from where etc etc... for details please check 2.4.access-modifiers.ts
+}
